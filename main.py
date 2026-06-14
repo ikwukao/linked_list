684bc4549c2ee14442a567e86ce6c0047087b110
Ikwuka Okoye <ikwukao@proton.me>
2026-06-14 19:18:54 +0100

    chore(main.py): test linked list

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ class LinkedList:
 
     def __init__(self):
         self.length = 0
+        self.head = None
+
+    def is_empty(self):
+        return self.length == 0
+
+
+# if __name__ == '__name__':
+my_list = LinkedList()
+print(my_list.is_empty())

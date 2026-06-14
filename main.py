3c6393d3d959475cde3f487ae9b0f79e75f094f4
Ikwuka Okoye <ikwukao@proton.me>
2026-06-14 19:45:54 +0100

    chore(main.py): create the remove method

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 class LinkedList:
+    
     class Node:
         def __init__(self, element):
             self.element = element
@@ -13,14 +14,33 @@ class LinkedList:
 
     def add(self, element):
         node = self.Node(element)
+        
         if self.is_empty():
             self.head = node
         else:
             current_node = self.head
-            while current_node.next is not None:
+           
+           while current_node.next is not None:
                 current_node = current_node.next
             current_node.next = node
+        
         self.length += 1
+
+    def remove(self, element):
+        previous_node = None
+        current_node = self.head
+        
+        while current_node is not None and current_node.element != element:
+            previous_node = current_node
+            current_node = current_node.next
+        
+        if current_node is None:
+            return
+        elif previous_node is not None:
+            previous_node.next = current_node.next
+        else:
+            self.head = current_node.next
+        self.length -= 1
 
 
 my_list = LinkedList()
@@ -28,6 +48,5 @@ print(my_list.is_empty())
 
 my_list.add(1)
 my_list.add(2)
-
 print(my_list.is_empty())
 print(my_list.length)
